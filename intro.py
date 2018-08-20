538b4c26934e08b1dd4e840601754ccd07f7761d
Leon.yan <yanyin1986@gmail.com>
2018-08-21 07:19:28 +0900

    my first commit with django girl

---
+++ intro.py
@@ -0,0 +1,15 @@
+def hi(name):
+    if name == 'Ola':
+        print('Hi Ola')
+    elif name == 'Sonja':
+        print('Hi Sonja')
+    else:
+        print('Hi anonymouse!')
+
+
+hi('')
+
+
+if 3 > 2:
+    print('It works!')
+print('hello, django girls')
